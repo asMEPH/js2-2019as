344715bfebcaa43f636e588ef0f7579264d6c824
asMEPH <joehero10@gmail.com>
2019-06-28 10:26:02 -0700

    aaaaaaaaaaaaaaa

--- src/app/haslo/haslo.component.ts
+++ src/app/haslo/haslo.component.ts
@@ -7,47 +7,92 @@ import { Router } from '@angular/router';
   styleUrls: ['./haslo.component.less']
 })
 export class HasloComponent implements OnInit {
+  m = new Map<string,string>();
+  s = 'btn-outline-warning';
   y = 'btn-outline-warning';
+  a = '';
+  login = '';
+  b = '';
 
 
   constructor(private router: Router) { }
 
-  LoginUser(e) {
 
-    var alias = e.target.elements[0].value ;
-    var password = e.target.elements[1].value ;
+  ngOnInit() {
+    this.m.set('admin', 'admin');
+    this.m.set('host', 'host');
+    // this.m.set('admin', this.b);
+    // this.m.set('host', this.b);
+  }
 
-    console.log(alias,password);
 
-    if(alias === 'host' && password === 'host'){
+  /*
+    loginUser(e) {
+
+      var alias = e.target.elements[0].value ;
+      var password = e.target.elements[1].value ;
+
+      console.log(alias,password);
+
+      if(alias === 'host' && password === 'host'){
+        this.router.navigate(['zalogowany']);
+      }
+      if(alias === 'admin' && password === 'admin'){
+        this.router.navigate(['zalogowany']);
+      }
+
+      else {
+        this.y = 'btn-outline-danger';
+      }
+    }
+    */
+
+  loginUser(){
+
+    /*let map = new Map<string,string>([
+      [ "host", "host"],
+      [ "admin", "admin"]
+    ]);
+
+    console.log(map);
+    for (let [key, value] of map.Map()) {
+      console.log(key, value);
+    }*/
+
+
+    console.log(this.m.get('admin'));
+    let pass = this.m.get(this.login);
+
+
+    // console.log(this.m.get('host'));
+
+    if(this.a===pass){
       this.router.navigate(['zalogowany']);
     }
-    if(alias === 'admin' && password === 'admin'){
+    if(this.a==='host' && this.login==='host'){
       this.router.navigate(['zalogowany']);
     }
+    // }
 
     else {
       this.y = 'btn-outline-danger';
     }
+
   }
 
-/*LoginUser(){
+  changeP() {
+    this.m.set('admin', this.b);
+    this.m.set('host', this.b);
 
-
-  let map = new Map<string,string>();
-  map.set("alias", "host");
-  map.set("password", "host");
-
-
-
-  console.log(map);
-
-
-}
-
-*/
-
-  ngOnInit() {
+    if(this.a==='admin' && this.login === 'admin' || this.a==='host' && this.login === 'host'){
+      this.m.set(this.a, this.b ); //nadpis
+      console.log(this.m.get('admin'));
+      console.log(this.m.get('host'))
+    }
+    else {
+      this.s = 'btn-outline-danger';
+    }
   }
+
 
 }
